2a6cdd83dafff3350aa89cb5ae1670e5007fa28f
Diego Orefici <orefici.diego@gmail.com>
2021-02-25 23:44:34 -0300

    Theme added.

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#03506f",
+        },
+        secondary: {
+            main: "#c24914",
+        },
+    },
+})
+
+export default theme
